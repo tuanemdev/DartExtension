954e34a92b2f44fe4fffb1e68ccf06af5a4e7815
Nguyen Tuan Anh <nguyentuananh.developer@gmail.com>
2025-04-20 10:43:50 +0700

    refactor

--- lib/string/multiline_string.dart
+++ lib/string/multiline_string.dart
@@ -1,9 +1,9 @@
-
 extension MultilineStringTrimmer on String {
   /// Trims leading whitespace from each line of a multiline string.</br>
-  /// Last line must contain only space and tab.</br>
-  /// refer: https://github.com/dart-lang/language/issues/559
+  /// The last line must contain only spaces and tabs to serve as the indentation pattern.</br>
+  /// See: https://github.com/dart-lang/language/issues/559
   String trimLeadingWhitespace() {
+    // Find the starting position of the indentation pattern (last line)
     int lastLineStart = length;
     int char = 0;
     while (lastLineStart > 0 && (char = codeUnitAt(lastLineStart - 1)) == 0x20 || char == 0x09) {
@@ -12,20 +12,24 @@ extension MultilineStringTrimmer on String {
     if (lastLineStart == 0) return "";
     StringBuffer buffer = StringBuffer();
     if (char != 0x0d && char != 0x0a) return this;
-    int i = 0;
+    
+    // Current position pointer in the string being processed
+    int currentPosition = 0;
     do {
-      for (var j = lastLineStart; j < length; j++) {
-        if (codeUnitAt(i) != codeUnitAt(j)) return this;
-        i++;
+      // Compare the beginning of the line with the indentation pattern
+      for (var indentPatternPosition = lastLineStart; indentPatternPosition < length; indentPatternPosition++) {
+        if (codeUnitAt(currentPosition) != codeUnitAt(indentPatternPosition)) return this;
+        currentPosition++;
       }
-      int lineStart = i;
+      
+      int lineStart = currentPosition;
       int char = 0;
-      while ((char = codeUnitAt(i++)) != 0x0a && char != 0x0d) { }
-      if (char == 0x0d && i < lastLineStart && codeUnitAt(i) == 0x0a) {
-        i++;
+      while ((char = codeUnitAt(currentPosition++)) != 0x0a && char != 0x0d) { }
+      if (char == 0x0d && currentPosition < lastLineStart && codeUnitAt(currentPosition) == 0x0a) {
+        currentPosition++;
       }
-      buffer.write(substring(lineStart, i));
-    } while (i < lastLineStart);
+      buffer.write(substring(lineStart, currentPosition));
+    } while (currentPosition < lastLineStart);
     return buffer.toString();
   }
 }
