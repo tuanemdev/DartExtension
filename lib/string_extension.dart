63f91ec5c88be7f25754b6f01d9433f8f71a3048
Nguyen Tuan Anh <nguyentuananh.developer@gmail.com>
2025-04-20 09:29:08 +0700

    init commit

---
+++ lib/string_extension.dart
@@ -0,0 +1,28 @@
+extension StringExtension on String {
+  String trimLeadingWhitespace() {
+    // Last line must contain only space and tab.
+    int lastLineStart = length;
+    int char = 0;
+    while (lastLineStart > 0 && (char = codeUnitAt(lastLineStart - 1) == 0x20) || char == 0x09) {
+      lastLineStart--;
+    }
+    if (lastLineStart == 0) return "";
+    StringBuffer buffer = StringBuffer();
+    if (char != 0x0d && char != 0x0a) return this;
+    int i = 0;
+    do {
+      for (var j = lastLineStart; j < length; j++) {
+        if (codeUnitAt(i) != codeUnitAt(j)) return this;
+        i++;
+      }
+      int lineStart = i;
+      int char = 0;
+      while ((char = codeUnitAt(i++)) != 0x0a && char != 0x0d) { }
+      if (char == 0x0d && i < lastLineStart && codeUnitAt(i) == 0x0a) {
+        i++;
+      }
+      buffer.write(substring(lineStart, i));
+    } while (i < lastLineStart);
+    return buffer.toString();
+  }
+}
